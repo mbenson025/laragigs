fcf6941191bbbd232c2678725db03963aae166ab
mbenson025 <mbenson025@gmail.com>
2022-11-27 12:23:18 -0600

    factory test with faker data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,34 +22,36 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        Listing::create(  [
-            'title' => 'Backend Developer', 
-            'tags' => 'laravel, php, api',
-            'company' => 'Skynet Systems',
-            'location' => 'Newark, NJ',
-            'email' => 'email4@email.com',
-            'website' => 'https://www.skynet.com',
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
-          ]);
+        Listing::factory(6)->create();
 
-        Listing::create(  [
-            'title' => 'Laravel Developer', 
-            'tags' => 'laravel, vue, javascript',
-            'company' => 'Wayne Enterprises',
-            'location' => 'Gotham, NY',
-            'email' => 'email3@email.com',
-            'website' => 'https://www.wayneenterprises.com',
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
-          ]);
+        // Listing::create(  [
+        //     'title' => 'Backend Developer', 
+        //     'tags' => 'laravel, php, api',
+        //     'company' => 'Skynet Systems',
+        //     'location' => 'Newark, NJ',
+        //     'email' => 'email4@email.com',
+        //     'website' => 'https://www.skynet.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+        //   ]);
+
+        // Listing::create(  [
+        //     'title' => 'Laravel Developer', 
+        //     'tags' => 'laravel, vue, javascript',
+        //     'company' => 'Wayne Enterprises',
+        //     'location' => 'Gotham, NY',
+        //     'email' => 'email3@email.com',
+        //     'website' => 'https://www.wayneenterprises.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+        //   ]);
         
-        Listing::create(  [
-            'title' => 'Full-Stack Engineer',
-            'tags' => 'laravel, backend ,api',
-            'company' => 'Stark Industries',
-            'location' => 'New York, NY',
-            'email' => 'email2@email.com',
-            'website' => 'https://www.starkindustries.com',
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
-          ]);
+        // Listing::create(  [
+        //     'title' => 'Full-Stack Engineer',
+        //     'tags' => 'laravel, backend ,api',
+        //     'company' => 'Stark Industries',
+        //     'location' => 'New York, NY',
+        //     'email' => 'email2@email.com',
+        //     'website' => 'https://www.starkindustries.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+        //   ]);
     }
 }
