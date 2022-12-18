8a1346a242cccb80c2c653f287c4af5faa3ee836
mbenson025 <mbenson025@gmail.com>
2022-12-17 20:49:42 -0600

    implement alpinejs

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -3,7 +3,7 @@
     {{-- laravel color
         px - padding on x axis 
         py - padding on y axis --}}
-    <div class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-3">
+    <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show" class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-3">
         <p>
             {{session('message')}}
         </p>
