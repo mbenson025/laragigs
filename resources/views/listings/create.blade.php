79da93db6a2b6f136c8fba3ffe295772a93f6a93
mbenson025 <mbenson025@gmail.com>
2022-12-17 21:06:04 -0600

    old function added in blade to save previously entered fields when form doesnt submit

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -18,7 +18,7 @@
                             <input
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
-                                name="company"
+                                name="company" value="{{old('company')}}"
                             />
 
                             @error('company')
@@ -35,7 +35,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="title"
-                                placeholder="Example: Senior Laravel Developer"
+                                placeholder="Example: Senior Laravel Developer" value="{{old('title')}}"
                             />
 
                             @error('title')
@@ -53,7 +53,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="location"
-                                placeholder="Example: Remote, Boston MA, etc"
+                                placeholder="Example: Remote, Boston MA, etc" value="{{old('location')}}"
                             />
 
                             @error('location')
@@ -68,7 +68,7 @@
                             <input
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
-                                name="email"
+                                name="email" value="{{old('email')}}"
                             />
 
                             @error('email')
@@ -86,7 +86,7 @@
                             <input
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
-                                name="website"
+                                name="website" value="{{old('website')}}"
                             />
 
                             @error('website')
@@ -102,7 +102,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="tags"
-                                placeholder="Example: Laravel, Backend, Postgres, etc"
+                                placeholder="Example: Laravel, Backend, Postgres, etc" value="{{old('tags')}}"
                             />
 
                             @error('tags')
@@ -124,16 +124,17 @@
                         <div class="mb-6">
                             <label
                                 for="description"
-                                class="inline-block text-lg mb-2"
-                            >
+                                class="inline-block text-lg mb-2">
                                 Job Description
                             </label>
                             <textarea
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="description"
                                 rows="10"
-                                placeholder="Include tasks, requirements, salary, etc"
-                            ></textarea>
+                                placeholder="Include tasks, requirements, salary, etc">
+                                {{-- old function goes outside div for textarea --}}
+                                {{old('description')}}
+                        </textarea>
 
                             @error('description')
                                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
